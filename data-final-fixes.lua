29401b4737e2ccd20e349a36c8475b68a62ce2ec
Youshida-sama <35883698+Youshida-sama@users.noreply.github.com>
2024-10-23 21:57:07 +0300

    added energy settings

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,10 +1,18 @@
 local l_speed = settings.startup["logistic-robot-speed"].value
+local l_energy_pt = settings.startup["logistic-robot-energy-tick"].value
+local l_energy_pm = settings.startup["logistic-robot-energy-move"].value
 local c_speed = settings.startup["construction-robot-speed"].value
+local c_energy_pt = settings.startup["construction-robot-energy-tick"].value
+local c_energy_pm = settings.startup["construction-robot-energy-move"].value
 
 for _, bot in pairs(data.raw["logistic-robot"]) do
     bot.speed = 0.05 * l_speed
+    bot.energy_per_tick = l_energy_pt
+    bot.energy_per_move = l_energy_pm
 end
 
 for _, bot in pairs(data.raw["construction-robot"]) do
     bot.speed = 0.05 * c_speed
+    bot.energy_per_tick = c_energy_pt
+    bot.energy_per_move = c_energy_pm
 end
--- settings.lua
+++ settings.lua
@@ -15,5 +15,33 @@ data:extend(
         setting_type = "startup",
         minimum_value = 0.5,
         default_value = 1
+    },
+    {
+        type = "string-setting",
+        name = "logistic-robot-energy-tick",
+        localised_name = "Logistic robot energy per tick",
+        setting_type = "startup",
+        default_value = "0.05kJ"
+    },
+    {
+        type = "string-setting",
+        name = "logistic-robot-energy-move",
+        localised_name = "Logistic robot energy per move",
+        setting_type = "startup",
+        default_value = "5kJ"
+    },
+    {
+        type = "string-setting",
+        name = "construction-robot-energy-tick",
+        localised_name = "Construction robot energy per tick",
+        setting_type = "startup",
+        default_value = "0.05kJ"
+    },
+    {
+        type = "string-setting",
+        name = "construction-robot-energy-move",
+        localised_name = "Construction robot energy per move",
+        setting_type = "startup",
+        default_value = "5kJ"
     }
 })
